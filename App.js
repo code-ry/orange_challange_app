d3a443ceb423ff17b22dc9daa255acc713dbc683
code-ry <rybussey@gmail.com>
2023-04-19 14:00:51 +1000

    initial commit

--- App.js
+++ App.js
@@ -2,14 +2,8 @@ import { View, Text } from "react-native";
 
 export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Universal React with Expo</Text>
+    <View>
+      <Text>Orange Challenge App!</Text>
     </View>
   );
 }
